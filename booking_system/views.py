b98b395639675ac1962c435391cb50eb383645a2
keshii123 <keshanimahanama@gmail.com>
2025-04-22 21:00:37 +0530

    some small changes

--- booking_system/views.py
+++ booking_system/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'booking_system/home.html')
+
 
 from django.shortcuts import render, redirect
 from django.contrib import messages

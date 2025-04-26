f54c5bff6c7a94ef85b97836efa4dfcdc3c6283b
mitilk <mitilk@yahoo.com>
2025-04-26 20:51:44 +0530

    fixed errors

--- booking_system/forms.py
+++ booking_system/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import UserProfile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -9,4 +10,16 @@ class UserRegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'phone_number', 'gender', 'password1','password2']
+        fields = ['username', 'email', 'phone_number', 'gender', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+            UserProfile.objects.create(
+                user=user,
+                phone_number=self.cleaned_data['phone_number'],
+                gender=self.cleaned_data['gender']
+            )
+        return user
